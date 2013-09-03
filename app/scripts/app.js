6baf52aff2f16ed1429d86886ea3ff9ac2efa9d3
Steve Zimmers <szimmers@digitalprimates.net>
2013-09-03 10:57:41 -0500

    checkpoint before refactoring services so getById is async and calls get if need be
    doesn't work currently.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sapmobileApp', ['sapmobileApp.ProjectService', 'sapmobileApp.EndpointService'])
-  .config(function ($routeProvider/*, Endpoints*/) {
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -11,8 +11,15 @@ angular.module('sapmobileApp', ['sapmobileApp.ProjectService', 'sapmobileApp.End
             templateUrl: 'views/projectDetail.html',
             controller: 'ProjectDetailCtrl',
 			resolve: {
-				endpointsRequired: ['Endpoints', '$q', function(Endpoints, $q) {
+				ensureEndpointsLoaded: ['Endpoints', '$q', function(Endpoints, $q) {
 					return Endpoints.resolve($q);
+				}],
+				project: ['Projects', '$route', function(Projects, $route){
+					var projectId = $route.current.params.projectId;
+					return Projects.getById(projectId);
+				}],
+				endpointForProject: ['Endpoints', 'project', function(Endpoints, project){
+					return Endpoints.getById(project.uniqueId);
 				}]
 			}
         })
--- app/scripts/controllers/ProjectDetail.js
+++ app/scripts/controllers/ProjectDetail.js
@@ -6,9 +6,9 @@ angular.module('sapmobileApp')
  * object. We also need to get the associated endpoint in order to get the correct host
  * to load the site tickets.
  */
-  .controller('ProjectDetailCtrl', function ($scope, $routeParams, Projects, Endpoints, ProjectTickets) {
+  .controller('ProjectDetailCtrl', function ($scope, $routeParams, Projects, Endpoints, ProjectTickets, project, endpointForProject) {
 		var projectId = $routeParams.projectId;
-		var project = Projects.getById(projectId);
+		//var project = Projects.getById(projectId);
 		var endpoint = Endpoints.getById(project.endpointId);
 		var endpointHost = endpoint.host;
 
