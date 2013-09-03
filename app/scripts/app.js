5643dfb55e22ed224263a6f8d13eeaa122e70dfd
Steve Zimmers <szimmers@digitalprimates.net>
2013-09-03 16:59:56 -0500

    - move auth from each service, put in app config - remove x-requested-with for CORS (got rid of that in keyser web configs)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,27 @@
 'use strict';
 
 angular.module('sapmobileApp', ['sapmobileApp.ProjectService'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/project/detail/:projectId', {
-            templateUrl: 'views/projectDetail.html',
-            controller: 'ProjectDetailCtrl',
-			resolve: {
-				project: ['Projects', '$route', function(Projects, $route){
-					var projectId = $route.current.params.projectId;
-					return Projects.getById(projectId);
-				}]
-			}
-        })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.config(function($httpProvider) {
+		$httpProvider.defaults.headers.common['Authorization'] = 'Basic YWR1bW1lcjp0ZXN0MTIz';
+		delete $httpProvider.defaults.headers.common["X-Requested-With"];
+	})
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl'
+			})
+			.when('/project/detail/:projectId', {
+				templateUrl: 'views/projectDetail.html',
+				controller: 'ProjectDetailCtrl',
+				resolve: {
+					project: ['Projects', '$route', function(Projects, $route){
+						var projectId = $route.current.params.projectId;
+						return Projects.getById(projectId);
+					}]
+				}
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
