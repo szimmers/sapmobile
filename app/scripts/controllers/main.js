c6898ba396ac08beb44477a2aeb7f1f44b12693a
Steve Zimmers <szimmers@digitalprimates.net>
2013-09-05 12:46:51 -0500

    filter out new project from project list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,4 +9,12 @@ angular.module('siteTicketPortal')
 		$scope.openProjectDetail = function(project) {
 			$location.path("/project/detail/" + project.uniqueId);
 		}
+
+		// hide new projects, since we can't access their tickets
+		$scope.hideNewStatus = function (project) {
+			if (project.status == 1)
+				return false;
+
+			return true;
+		};
 	});
