751c469697815dadc02bd8b489a85ad0fbbe1713
Steve Zimmers <szimmers@digitalprimates.net>
2013-09-09 15:41:49 -0500

    chart cleanup
    - create filter for creating histogram
    - clean up some chart sizing/styling

---
+++ app/scripts/filters/statusDataHistogramForSiteTickets.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('siteTicketPortal')
+/**
+ * filter for transforming ticket data into histogram object that provides a count (statusCount) for each
+ * unique siteWorkItem.status (statusVal).
+ */
+	.filter('statusDataHistogramForSiteTicket', function () {
+		return function (tickets) {
+			var histogram = {};
+
+			for (var i=0; i < tickets.length; i++) {
+
+				var statusVal = tickets[i].siteWorkItem.status;
+
+				if (histogram[statusVal] == undefined) {
+					histogram[statusVal] = {};
+					histogram[statusVal].statusCount = 0;
+				}
+
+				histogram[statusVal].statusCount++;
+			}
+
+			return histogram;
+		};
+	});
