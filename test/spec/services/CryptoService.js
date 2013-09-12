407c44a9103705ce4f76b05af3c2a109beee45f0
Steve Zimmers <szimmers@digitalprimates.net>
2013-09-12 16:34:29 -0500

    unit tests
    - finish creating tests for services

--- test/spec/services/CryptoService.js
+++ test/spec/services/CryptoService.js
@@ -2,14 +2,24 @@
 
 describe('Service: CryptoService', function () {
 
-  // load the service's module
-  beforeEach(module('sapmobileApp'));
+	// load the service's module
+	beforeEach(module('siteTicketPortal'));
+	beforeEach(module('siteTicketPortal.CryptoService'));
 
-  // instantiate service
-  var CryptoService;
-  beforeEach(inject(function (_CryptoService_) {
-    CryptoService = _CryptoService_;
-  }));
+	// instantiate service
+	var service, username, password, authString;
 
+	beforeEach(inject(function ($injector) {
+
+		username = 'larry';
+		password = 'larry123';
+		authString = 'Basic bGFycnk6bGFycnkxMjM=';
+
+		service = $injector.get('Crypto');
+	}));
+
+	it('should create a basic auth string', inject(function () {
+		expect(service.getAuthString(username, password)).toBe(authString);
+	}));
 
 });
--- test/spec/services/SiteService.js
+++ test/spec/services/SiteService.js
@@ -2,14 +2,48 @@
 
 describe('Service: SiteService', function () {
 
-  // load the service's module
-  beforeEach(module('siteTicketPortal'));
+	beforeEach(module('siteTicketPortal'));
+	beforeEach(module('siteTicketPortal.SiteService'));
 
-  // instantiate service
-  var SiteService;
-  beforeEach(inject(function (_SiteService_) {
-    SiteService = _SiteService_;
-  }));
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
+	});
+
+	var service, $httpBackend, scope, returnData, brandKey, siteIdentifier;
+
+	beforeEach(inject(function ($injector, $rootScope, BASE_URL) {
+
+		brandKey = 'BAR';
+		siteIdentifier = '54321';
+
+		// service returns an array
+		returnData = [
+			{"address1":"168 N. Pinest Ave", "address2":"", "altPhone":"", "brandKey":"BAR", "city":"MEMPHIS",
+			"emailAddress":"", "primaryPhone":"909-345-9253", "siteIdentifier":"54321", "state":"TN", "zip":"38116 "}
+		];
+
+		var url = BASE_URL + '/services/sites/?brand=' + brandKey + '&site=' + siteIdentifier;
+
+		scope = $rootScope.$new();
+
+		$httpBackend = $injector.get('$httpBackend');
+		$httpBackend.expectGET(url).respond(200, returnData);
+
+		service = $injector.get('Site');
+	}));
+
+	it('should return the site', inject(function () {
+
+		var promise = service.get(brandKey, siteIdentifier).then(function(response) {
+			scope.data = response;
+		});
+
+		$httpBackend.flush();
+		scope.$apply();
+
+		expect(scope.data).toEqual(returnData[0]);
+	}));
 
 
 });
